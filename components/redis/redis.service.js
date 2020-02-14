1fb99c310247a86c543366548735b12a31909019
Brajendra Pratap <pratap.singh2604@gmail.com>
2020-02-15 01:20:47 +0530

    added dupe check in redis

--- components/redis/redis.service.js
+++ components/redis/redis.service.js
@@ -1,7 +1,13 @@
 import { redisClient } from './redis.config.js'
 import { redisModel } from '../db/mongo.model.js'
+import uuid from 'uuid/v1.js';
+import { promisify } from 'util'
 
 const publishClient = redisClient();
+const client = redisClient();
+
+const getAsync = promisify(client.get).bind(client)
+const setAsync = promisify(client.set).bind(client);
 
 export function subscribe(client, channel) {
   if(!client) throw new Error(`Invalid Configuration`);
@@ -12,22 +18,36 @@ export function subscribe(client, channel) {
 export function readAndPublish(client, incomingChannel) {
 
   // read incoming message
-  client.on("pmessage", function (pattern, channel, message) {
+  client.on("pmessage", async function (pattern, channel, message) {
     
     if(channel === incomingChannel) {
       const stringifiedMessage  = JSON.stringify({ originalChannel: channel, originalMessage: message })
   
       publishToDb(stringifiedMessage);
     } else if(channel === 'orchestrator-channel') {
-      const msgObj = new redisModel({
-        originalChannel: JSON.parse(message).originalChannel, 
-        originalMessage: JSON.parse(message).originalMessage, 
-      })
-      // writing to db
-      msgObj.save(msgObj);
+      console.log('in orch channel')
+      const parsedMessage = await setValueInRedis(message);
+      if(parsedMessage) {
+        const msgObj = new redisModel({
+          originalChannel: JSON.parse(message).originalChannel, 
+          originalMessage: JSON.parse(message).originalMessage, 
+        })
+        // writing to db
+        msgObj.save();
+      }
     }
-
   })
+}
+
+async function setValueInRedis(message) {
+  const parsedMessage = JSON.parse(message);
+  
+  if(!(await getAsync(parsedMessage.originalMessage))) {
+    await setAsync(parsedMessage.originalMessage, uuid());
+    client.expire(parsedMessage.originalMessage, 10);
+    return parsedMessage;
+  }
+  return false;
 }
 
 function publishToDb(msg) {
